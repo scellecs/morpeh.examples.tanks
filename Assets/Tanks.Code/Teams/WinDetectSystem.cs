5f677d95b44010b2bde580b25d56d8b6baf6e89b
SH42913 <shoot913@gmail.com>
2022-01-10 12:22:19 +0300

    WinDetectSystem will show win-message

--- Assets/Tanks.Code/Teams/WinDetectSystem.cs
+++ Assets/Tanks.Code/Teams/WinDetectSystem.cs
@@ -1,28 +1,48 @@
 ﻿namespace Tanks.Teams {
     using Morpeh;
+    using Morpeh.Helpers;
     using UnityEngine;
+    using UtilSystems;
 
     [CreateAssetMenu(menuName = "ECS/Systems/" + nameof(WinDetectSystem))]
     public sealed class WinDetectSystem : UpdateSystem {
         public float winTimeScale = 0.1f;
+        public TextInWorldSystem.Request winMessageRequest;
+
         private Filter nonLosingTeams;
+        private Filter winMarkers;
 
         public override void OnAwake() {
             nonLosingTeams = World.Filter.With<Team>().Without<LosingTeam>();
+            winMarkers = World.Filter.With<WinMarker>();
         }
 
         public override void OnUpdate(float deltaTime) {
-            if (nonLosingTeams.Length > 1) {
+            if (!winMarkers.IsEmpty()) {
                 return;
             }
 
-            ref Team winTeam = ref nonLosingTeams.GetEntity(0).GetComponent<Team>();
-            Debug.Log($"Team {winTeam.name} wins!");
+            if (nonLosingTeams.IsEmpty() || nonLosingTeams.Length > 1) {
+                return;
+            }
+
+            ref Team winTeam = ref nonLosingTeams.First().GetComponent<Team>();
+            var text = $"Team {winTeam.name} wins!";
+            Debug.Log(text);
+
+            TextInWorldSystem.Request request = winMessageRequest;
+            request.color = winTeam.color;
+            request.text = text;
+            World.CreateEntity().SetComponent(request);
+
             Time.timeScale = winTimeScale;
+            World.CreateEntity().AddComponent<WinMarker>();
         }
 
         public static WinDetectSystem Create() {
             return CreateInstance<WinDetectSystem>();
         }
+
+        public struct WinMarker : IComponent { }
     }
 }
